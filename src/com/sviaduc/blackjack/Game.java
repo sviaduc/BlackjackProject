8b835c44bd9e3bd4761c70a2ecfe4aa8f60faab4
Stanton Viaduc <stan@Stantons-MacBook-Pro.local>
2017-08-20 11:18:15 -0600

    added win method to game logic

--- src/com/sviaduc/blackjack/Game.java
+++ src/com/sviaduc/blackjack/Game.java
@@ -12,67 +12,87 @@ public class Game {
 	public void run() {
 		Deck deck = initializeDeck();
 		playerTurn(deck);
-	
 		}
 	
 	private void playerTurn(Deck deck) {
 		player.getHand().addCard(deck.dealCard());
 		player.getHand().addCard(deck.dealCard());
+		dealer.getHand().addCard(deck.dealCard());
+		dealer.getHand().addCard(deck.dealCard());
+		
 		System.out.println("Card counter: " + deck.getCard().size());
 		int playerHandValue = 0;
-		for (Card c : player.getHand().getCards()) {
-		System.out.println(c);	
-		playerHandValue = playerHandValue + c.getRank().getValue();
-		
+		int dealerHandValue = 0;
+		for (Card p : player.getHand().getCards()) {
+		System.out.println(p);	
+		playerHandValue = playerHandValue + p.getRank().getValue();
+		}
+		for(Card d : dealer.getHand().getCards()) {
+			System.out.println("Dealers " + d);
+			dealerHandValue = dealerHandValue + d.getRank().getValue();
 		}
 		System.out.println("Player's Hand: " + playerHandValue);
 		
-		if(playerHandValue == 21) {
+		
+	
+		if(playerHandValue == 21 && dealerHandValue < 21) {
 			System.out.println("Blackjack!!");
-			dealerTurn(deck);
-		}
-		
-		while(playerHandValue < 21){
-		Scanner kb = new Scanner(System.in);
-		System.out.println("Do you want to: ");
-		System.out.println("(1)Hit");
-		System.out.println("(2)Stand");
-		int choice = kb.nextInt();
-		
-		switch(choice) {
-		case 1:
-		player.getHand().addCard(deck.dealCard());
-		System.out.println("Card counter: " + deck.getCard().size());
-		int playerHandValue1 = 0;
-		for (Card c : player.getHand().getCards()) {
-		System.out.println(c);	
-		playerHandValue1 = playerHandValue1 + c.getRank().getValue();
-		playerHandValue = playerHandValue + playerHandValue1;
-		System.out.println(playerHandValue);
-		}
-			break;
-		case 2:
-			dealerTurn(deck);
-			break;
 			
 		}
-		
+		else if(playerHandValue == 21 && dealerHandValue==21) {
+			System.out.println("Dealer and Player push");
 		}
-		if(playerHandValue > 21) {
-			System.out.println("You Busted!");
+		else if(dealerHandValue==21 && playerHandValue<21) {
+			System.out.println("Dealer BlackJack");
+		}
+		else{
+			boolean sLoop = true;
+			while(playerHandValue < 21 && sLoop == true){
+			Scanner kb = new Scanner(System.in);
+			System.out.println("Do you want to: ");
+			System.out.println("(1)Hit");
+			System.out.println("(2)Stand");
+			int choice = kb.nextInt();
+			
+				switch(choice) {
+				case 1:
+				player.getHand().addCard(deck.dealCard());
+				System.out.println("Card counter: " + deck.getCard().size());
+				int playerHandValue1 = 0;
+					for (Card c : player.getHand().getCards()) {
+					System.out.println(c);	
+					playerHandValue1 = playerHandValue1 + c.getRank().getValue();
+					System.out.println(playerHandValue1);
+					playerHandValue = playerHandValue1;
+						if(playerHandValue == 21) {
+							dealerTurn(deck, dealerHandValue);
+							checkWin(dealerHandValue, playerHandValue);
+						}
+					}
+					
+					break;
+				case 2:
+					dealerHandValue = dealerTurn(deck, dealerHandValue);
+					checkWin(dealerHandValue, playerHandValue);
+					sLoop = false;
+					break;
+					
+				}
+			
+			}
 		}
 		
 	}	
 		
-	private void dealerTurn(Deck deck) {
+	
+	private int dealerTurn(Deck deck, int dealerHandValue) {
 		System.out.println("*********Dealers Hand*********");
-		dealer.getHand().addCard(deck.dealCard());
-		dealer.getHand().addCard(deck.dealCard());
+		//dealer.getHand().addCard(deck.dealCard());
 		System.out.println("Card counter: " + deck.getCard().size());
-		int dealerHandValue = 0;
-		for (Card c : dealer.getHand().getCards()) {
-			System.out.println(c);	
-			dealerHandValue = dealerHandValue + c.getRank().getValue();
+		//int dealerHandValue = 0;
+		for (Card d : dealer.getHand().getCards()) {
+			System.out.println(d);	
+			dealerHandValue = dealerHandValue + d.getRank().getValue();
 			
 		}
 		System.out.println("Dealer's Hand: " + dealerHandValue);
@@ -81,15 +101,38 @@ public class Game {
 			dealerHandValue = 0;
 			dealer.getHand().addCard(deck.dealCard());
 			System.out.println("Card counter: " + deck.getCard().size());
-			for (Card c : dealer.getHand().getCards()) {
-				System.out.println(c);	
-				dealerHandValue = dealerHandValue + c.getRank().getValue();
+			for (Card d : dealer.getHand().getCards()) {
+				System.out.println(d);	
+				dealerHandValue = dealerHandValue + d.getRank().getValue();
+			}
+			if(dealerHandValue > 21) {
+				System.out.println("Dealer Busted!!!");
 			}
 			System.out.println("Dealer's Hand: " + dealerHandValue);
+			
 		}
-		
+		return dealerHandValue;
 	}
 
+	private Boolean checkWin(int dealerHandValue, int playerHandValue) {
+		
+		if(dealerHandValue >21 && playerHandValue<=21) {
+			System.out.println("Dealer busted");
+		}
+		else if(dealerHandValue <= 21 && playerHandValue>21) {
+			System.out.println("Player Busted");
+		}
+		else if(dealerHandValue>playerHandValue) {
+			System.out.println("Dealer wins");
+		}
+		else if(dealerHandValue<playerHandValue) {
+			System.out.println("Player Wins");
+		}
+		else if (dealerHandValue==playerHandValue){
+			System.out.println("Push...");
+		}
+		return true;
+	}
 	public Deck initializeDeck() {
 		Deck deck = new Deck();
 		
